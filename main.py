f62624b860a72be1afca4ed1761e10cfc1ffe74d
Decxo360 <diegolundstedt99@gmail.com>
2023-01-18 17:19:46 -0300

    nueva ruta de seguidor

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from model.Usuario import User
 from sqlalchemy import Select
 from sqlalchemy.orm import Session
-from routes import usuario
+from routes import usuario,seguidor
 from config.bd import db,engine
 
 app = FastAPI()
@@ -10,3 +10,4 @@ app = FastAPI()
 session = Session(engine)
 
 app.include_router(usuario.router)
+app.include_router(seguidor.router)
