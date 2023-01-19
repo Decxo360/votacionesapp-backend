5500b713b5900a2461b0660819723b789a619971
Decxo360 <diegolundstedt99@gmail.com>
2023-01-19 20:00:11 -0300

    ruta de votos agregado

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from sqlalchemy.orm import Session
-from routes import usuario,seguidor,votacion
+from routes import usuario,seguidor,votacion,voto
 from config.bd import engine
 
 app = FastAPI()
@@ -10,3 +10,4 @@ session = Session(engine)
 app.include_router(usuario.router)
 app.include_router(seguidor.router)
 app.include_router(votacion.router)
+app.include_router(voto.router)
