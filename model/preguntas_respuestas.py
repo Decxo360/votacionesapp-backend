47a492b6bf6017335da610c8d0b6dbb7c52a6cdf
Decxo360 <diegolundstedt99@gmail.com>
2023-01-18 21:34:26 -0300

    modificacion e integracion de una primarykey

--- model/preguntas_respuestas.py
+++ model/preguntas_respuestas.py
@@ -6,11 +6,12 @@ from sqlalchemy.orm import Mapped
 
 Base = declarative_base()
 
-class PreguntasResuestas(Base):
+class PreguntasRespuestas(Base):
     __tablename__ = 'preguntas_respuestas'
 
+    idpreguntarespuesta:Mapped[int] = Column(primary_key=True)
     id_pregunta:Mapped[int] = Column(ForeignKey('preguntas.idpreguntas'));
-    id_respuesta:Mapped[int] = Column(ForeignKey('respuesta.respuesta'));
+    id_respuesta:Mapped[int] = Column(ForeignKey('respuesta.idrespuesta'));
 
     def __repr__(self) -> str:
         return f'PreguntasRespuestas(id_pregunta={self.id_pregunta},id_respuesta={self.id_respuesta})'
