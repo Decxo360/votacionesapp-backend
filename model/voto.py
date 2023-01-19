4a95021f657ae6e0564b910e69b9eb8acee29bc2
Decxo360 <diegolundstedt99@gmail.com>
2023-01-19 20:00:31 -0300

    agregar primary key

--- model/voto.py
+++ model/voto.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 class Voto(Base):
     __tablename__ = 'voto'
 
+    idvoto:Mapped[int] = Column(primary_key=True)
     id_usuario:Mapped[int] = Column(ForeignKey("usuario.idrespuesta"))
     id_votacion:Mapped[int] = Column(ForeignKey("votacion.idrespuesta"))
     id_pregunta:Mapped[int] = Column(ForeignKey("preguntas.idrespuesta"))
